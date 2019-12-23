2e8abe606b0ec13a1bc6325a8983696317a39941
Caroline <55096375+swtsxmrr@users.noreply.github.com>
2019-12-24 02:37:39 +0300

    Add files via upload

---
+++ Лаб.раб. 21/Код Лабораторной 21/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Код_Лабораторной_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1 ===============================
+            /*
+            char[] S;
+            Console.Write("Введите строку: ");
+            S = Console.ReadLine().ToCharArray();
+            int k = 1;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if ((S[i] == ' ') && (S[i - 1] != ' '))
+                {
+                    k++;
+                }
+            }
+            Console.Write("Количество слов в строке: " + k);
+            Console.ReadKey();
+            */
+            //Задание 2 ===============================
+            /*
+            string S;
+            Console.Write("Введите строку: ");
+            S = Console.ReadLine();
+            string W = (from min in S.Split(' ')
+                              orderby min.Length
+                              select min).First();
+            Console.Write("Длина самого короткого слова в строке: ");
+            Console.Write(W.Length);
+            Console.ReadKey();
+            */
+            //Задание 3 ===============================
+            /*
+            char[] S;
+            char c;
+            Console.Write("Введите строку: ");
+            S = Console.ReadLine().ToArray();
+            c = S[0];
+            for (int i = 1; i < S.Length; i++)
+            {
+                if (S[i] == c)
+                {
+                    S[i] = '.';
+                }
+            }
+            Console.Write("Преобразованная строка: ");
+            Console.Write(S);
+            Console.ReadKey();
+            */
+            //Задание 4 ===============================
+            /*
+            string S;
+            char[] GL = { 'А', 'а', 'Е', 'е', 'Ё', 'ё', 'И', 'и', 'О', 'о', 'У', 'у', 'ы', 'Э', 'э', 'Ю', 'ю', 'Я', 'я' };
+            Console.Write("Введите строку: ");
+            S = Console.ReadLine();
+            int k = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                for (int j = 0; j < GL.Length; j++)
+                {
+                    if (S[i] == GL[j])
+                    {
+                        k++;
+                    }
+                }
+            }
+            Console.Write("Количество гласных в строке: ");
+            Console.Write(k);
+            Console.ReadKey();
+            */
+            //Задание 5 ===============================
+            //C:\Лабы C#\Лаб.раб. 21\Код Лабораторной 21
+            /*
+            string FullPath;
+            Console.Write("Введите полный путь к файлу: ");
+            FullPath = Console.ReadLine();
+            Console.Write("Имя файла: ");
+            Console.Write(Path.GetFileNameWithoutExtension(FullPath));
+            Console.ReadKey();
+            */
+            //Задание 6 ===============================
+            //C:\Лабы C#\Лаб.раб. 21\Код Лабораторной 21
+            /*
+            string FullPath;
+            Console.Write("Введите полный путь к файлу: ");
+            FullPath = Console.ReadLine();
+            if ((char)(FullPath.Split('\\').Length) > 2)
+            {
+                Console.Write("Имя файла: ");
+                Console.WriteLine(FullPath.Split('\\')[FullPath.Split('\\').Length - 2]);
+            }
+            else Console.Write("Имя файла: \\ ");
+            Console.ReadKey();
+            */
+            //Задание 7 ===============================
+            /*
+            string S;
+            Console.Write("Введите строку: ");
+            S = Console.ReadLine();
+            Console.Write("Преобразованная строка: ");
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(S[i]);
+                }
+            }
+            for (int i = S.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(S[i]);
+                }
+            }
+            Console.ReadKey();
+            */
+        }
+    }
+}
---
+++ Лаб.раб. 22/Код Лабораторной 22/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Код_Лабораторной_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1 =====================================================
+            //удаление первого слова
+            /*
+            string path = @"C:\Лабы C#\Лаб.раб. 22\Лабораторная 22.txt";
+            string text = File.ReadAllText(path);
+            Console.Write("Строка в файле: ");
+            Console.Write(text);
+            Console.WriteLine();
+            text = text.Remove(0, text.IndexOf(' ') + 1);
+            File.WriteAllText(path, text);
+            Console.Write("Исправленная строка в файле: ");
+            Console.Write(text);
+            Console.ReadKey();
+            */
+            //Задание 2 =====================================================
+            //звездочки
+            /*
+            string path = @"C:\Лабы C#\Лаб.раб. 22\Лабораторная 22.txt";
+            string text = File.ReadAllText(path);
+            Console.Write("Введите число K (кол-во звезд): ");
+            int K = int.Parse(Console.ReadLine());
+            Console.Write("Введите число N (строк): ");
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < K; j++)
+                {
+                    File.AppendAllText(path, string.Join(path, " * "));
+                }
+                File.AppendAllText(path, Environment.NewLine);
+            }
+            Console.ReadKey();
+            */
+            //Задание 3 =====================================================
+            //в начало первого содержимое второго
+            /*
+            string path1 = @"C:\Лабы C#\Лаб.раб. 22\Лабораторная 22.txt";
+            string path2 = @"C:\Лабы C#\Лаб.раб. 22\Лабораторная 22 еще.txt";
+            string text1 = File.ReadAllText(path1);
+            string text2 = File.ReadAllText(path2);
+            Console.Write("Строка в первом файле: ");
+            Console.Write(text1);
+            Console.WriteLine();
+            Console.Write("Строка во втором файле: ");
+            Console.Write(text2);
+            File.WriteAllText(path1, File.ReadAllText(path2) + text1);
+            Console.ReadKey();
+            */
+
+            //Задание 4 =====================================================
+            //подряд идущие пробелы
+            /*
+            string path = @"C:\Лабы C#\Лаб.раб. 22\Лабораторная 22.txt";
+            string text = File.ReadAllText(path);
+            Console.Write("Строка в файле: ");
+            Console.Write(text);
+            File.Delete(path);
+            while (text.Contains("  "))
+            {
+                text = text.Replace("  ", " ");
+            }
+            File.AppendAllText(path, text);
+            Console.ReadKey();
+            */
+            //Задание 5 =====================================================
+            //5 пробелов - абзац
+            /*
+            string path = @"C:\Лабы C#\Лаб.раб. 22\Лабораторная 22.txt";
+            string text = File.ReadAllText(path);
+            string pattern = @"     ";
+            int k = 0;
+            Regex space = new Regex(pattern);
+            MatchCollection matches = space.Matches(text);
+            foreach (Match mat in matches)
+            {
+                k++;
+            }
+            Console.Write("Количество абзацев: ");
+            Console.Write(k);
+            Console.ReadKey();
+            */
+
+        }
+    }
+}
